93ba5ab6314312431ff5821c12425aeb6ba1975c
Remap Development <mohammadrizkyk@gmail.com>
2019-09-04 12:44:05 +0700

    frontend

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from "react";
+import logo from './logo.svg';
+import './App.css';
+import HomeForm from "./components/form/HomeForm";
+import 'antd/dist/antd.css';
+import {Form} from "antd";
+
+function App() {
+    return (
+
+        <div className="App">
+            <br/><br/>
+            <h1>Validation</h1>
+            <HomeForm></HomeForm>
+        </div>
+    );
+}
+
+export default App;
